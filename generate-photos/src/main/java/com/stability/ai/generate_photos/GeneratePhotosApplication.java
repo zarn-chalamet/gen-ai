d1688e9821c9c4fd3e46d246569275151aa5385e
zarn_chalamet <6531503195@lamduan.mfu.ac.th>
2025-08-20 15:24:12 +0700

    initialize sb project

---
+++ generate-photos/src/main/java/com/stability/ai/generate_photos/GeneratePhotosApplication.java
@@ -0,0 +1,13 @@
+package com.stability.ai.generate_photos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratePhotosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneratePhotosApplication.class, args);
+	}
+
+}
---
+++ generate-photos/src/test/java/com/stability/ai/generate_photos/GeneratePhotosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stability.ai.generate_photos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneratePhotosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
