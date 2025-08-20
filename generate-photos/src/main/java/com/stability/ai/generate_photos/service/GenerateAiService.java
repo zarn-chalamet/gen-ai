dca87b83902d2feba1b443806d09bff5f6638ca1
zarn_chalamet <6531503195@lamduan.mfu.ac.th>
2025-08-21 00:04:18 +0700

    'fixes

--- generate-photos/src/main/java/com/stability/ai/generate_photos/service/GenerateAiService.java
+++ generate-photos/src/main/java/com/stability/ai/generate_photos/service/GenerateAiService.java
@@ -1,5 +1,6 @@
 package com.stability.ai.generate_photos.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
@@ -9,8 +10,14 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class GenerateAiService {
 
-    private static final String API_URL = "https://api.stability.ai/v2beta/stable-image/generate/core";
-    private static final String API_KEY = "sk-sMijdZLfROTeoYcNykHx9W4miBPR4DWsVw9gELKcB9pDFKjt";
+    private final String apiUrl;
+    private final String apiKey;
+
+    public GenerateAiService(@Value("${stability.api.key}") String apiKey) {
+        this.apiUrl = "https://api.stability.ai/v2beta/stable-image/generate/core";
+        this.apiKey = apiKey;
+    }
+
 
     public ResponseEntity<byte[]> generateTextToImage(String prompt) {
         RestTemplate restTemplate = new RestTemplate();
@@ -23,14 +30,14 @@ public class GenerateAiService {
         // Prepare headers
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-        headers.set("Authorization", "Bearer " + API_KEY);
+        headers.set("Authorization", "Bearer " + apiKey);
         headers.setAccept(MediaType.parseMediaTypes("image/*"));
 
         HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
 
         // Call Stability API
         ResponseEntity<byte[]> response = restTemplate.exchange(
-                API_URL,
+                apiUrl,
                 HttpMethod.POST,
                 requestEntity,
                 byte[].class
